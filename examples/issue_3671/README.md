c4af7981bf5a4e342a7a778e594f3d83396c67a2
Tommy Yu <y@metatoaster.com>
2025-03-07 14:05:42 +1300

    Initial commit of demo for #3617
    - Include the unmodified version of the supplied code along with the
      supporting standard main and lib.

---
+++ examples/issue_3671/README.md
@@ -0,0 +1,8 @@
+# Issue 3671 demo.
+
+The commit history for this example will show the variations between the
+different combinations of context/resource/signal use.
+
+## Quick Start
+
+Run `cargo leptos watch` to run this example.
---
+++ examples/issue_3671/src/app.rs
@@ -0,0 +1,72 @@
+use leptos::prelude::*;
+use leptos_meta::{MetaTags, *};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    path, SsrMode,
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <AutoReload options=options.clone()/>
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+    let fallback = || view! { "Page not found." }.into_view();
+    view! {
+        <Title text="Issue 3671 demo"/>
+        <Router>
+            <main>
+                <Routes fallback>
+                    <Route path=path!("") view=HomePage ssr=SsrMode::Async/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+#[server]
+async fn server_call() -> Result<(), ServerFnError> {
+    tokio::time::sleep(std::time::Duration::from_millis(50)).await;
+    Ok(())
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone)]
+struct Ctx(Option<()>);
+
+#[component]
+fn HomePage() -> impl IntoView {
+    let (_, set_ctx) = signal(Ctx(None));
+    provide_context(set_ctx);
+
+    // #[cfg(not(feature = "ssr"))]
+    on_cleanup(|| expect_context::<WriteSignal<Ctx>>().set(Ctx(None)));
+
+    let hook = move || {
+        let resource = Resource::new_blocking(
+            move || (),
+            move |_| async move { server_call().await },
+        );
+        Suspend::new(async move {
+            let _ = resource.await.map(|_| {
+                expect_context::<WriteSignal<Ctx>>().set(Ctx(Some(())))
+            });
+        })
+    };
+    view! {
+        <h1>"Home Page"</h1>
+        <Suspense>{hook}</Suspense>
+    }
+}
